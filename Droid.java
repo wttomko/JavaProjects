0a327c874a3f42a965c79d3b6999d7b9734b0e95
Tyler Tomko <145944847+wttomko@users.noreply.github.com>
2024-06-27 21:05:27 -0400

    Create Droid.java

---
+++ Droid.java
@@ -0,0 +1,39 @@
+public class Droid {
+    String name;
+    int batteryLevel;
+    public Droid(String name){
+        this.name = name;
+        batteryLevel=100;
+    }
+
+    public String toString(){
+        return "Greeting: My name is " + name;
+    }
+
+    public void performTask(String task) {
+        if (batteryLevel > 10){
+            System.out.println(name + " is performing task: " + task);
+            batteryLevel-=10;
+        } else {
+            System.out.println("Not enough battery life to perform this task.");
+        }
+    }
+
+    public void energyReport(){
+        System.out.println("Battery Level: " + batteryLevel);
+    }
+
+    public void recharge(){
+        batteryLevel=100;
+        System.out.println("Battery recharged!");
+    }
+
+    public static void main(String[] args){
+        Droid codey = new Droid("Codey");
+        codey.performTask("vacumn");
+        codey.performTask("Pick up groceries");
+        codey.energyReport();
+        codey.recharge();
+        codey.energyReport();
+    }
+}
