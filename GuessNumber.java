b3800f1fbb2de6548f0905440e3213fa80591dca
Tyler Tomko <145944847+wttomko@users.noreply.github.com>
2024-06-28 23:46:16 -0400

    added guess number and tictactoe
    tictactoe is not completed

---
+++ GuessNumber.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    int correctNum;
+    int guessAmt;
+    int playerguess;
+
+    public GuessNumber(int max){
+        System.out.println("Welcome to guess the number! Start by entering a number 0 through " + max);
+        Random ran = new Random();
+        this.correctNum=ran.nextInt(max);
+        this.guessAmt = 0;
+        this.playerguess = -1;
+    }
+
+    public String checkGuess(int guess){
+        this.guessAmt++;
+        if (guess < this.correctNum){
+            return "Higher";
+        } else if (guess > this.correctNum){
+            return "Lower";
+        } else {
+            return "Correct! It took you " + this.guessAmt + " guesses.";
+        }
+    }
+
+    public static void main(String[] args){
+        GuessNumber game = new GuessNumber(2000);
+        Scanner scan = new Scanner(System.in);
+        while (game.playerguess != game.correctNum){
+            game.playerguess = scan.nextInt();
+            System.out.println(game.checkGuess(game.playerguess));
+        }
+        scan.close();
+    }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,37 @@
+public class TicTacToe {
+    String[][] board = new String[3][3];
+    
+    int[][] winningCombos = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {1,4,7},
+        {2,5,8},
+        {3,6,9},
+        {1,5,9},
+        {3,5,7},
+    };
+
+    public TicTacToe(){
+
+        for (int x=0; x<board.length; x++){
+            for (int y=0; y<board[x].length; y++){
+                board[x][y] = " ";
+            }
+        }
+
+    }
+    public void displayBoard(){
+        for (int x=0; x<board.length; x++){
+            for (int y=0; y<board[x].length; y++){
+                System.out.print("[" + board[x][y] + "]" + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void main(String[] args){
+        TicTacToe TTT = new TicTacToe();
+        TTT.displayBoard();
+    }
+}
